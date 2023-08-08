62f4067994d76c79b51d2a9b71cae9f42d37ab0e
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2023-08-08 09:00:06 -0400

    feat(src): add starting code

---
+++ src/components/About/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function About(props) {
+    return <p>About</p>
+}
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function Hero(props) {
+    return <p>Hero</p>
+}
